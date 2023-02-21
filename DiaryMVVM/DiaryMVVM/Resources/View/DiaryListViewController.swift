25ed4809ce413bd2994e3a43d61c7cb1cad07f88
미니 <cow970814@naver.com>
2023-02-21 15:46:21 +0900

    :bricks: MVVM에 따른 폴더링 변경

--- DiaryMVVM/DiaryMVVM/Resources/View/DiaryListViewController.swift
+++ DiaryMVVM/DiaryMVVM/Resources/View/DiaryListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class DiaryListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
